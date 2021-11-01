648a1769cc58094fbd2ad45fe3915c4a38f91687
Aaron <aaron@ironmthome.com>
2021-10-31 23:15:33 -0400

    Add Services/* class tests

---
+++ tests/Feature/Services/ViewServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use PikaJew002\Handrolled\Application\Application;
+use PikaJew002\Handrolled\Application\Services\ViewService;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\Loader\LoaderInterface;
+
+beforeEach(function() {
+    $this->app = new Application('./tests/artifacts', './tests/artifacts/config');
+    $this->app->bootService(new ViewService($this->app));
+});
+
+it('registers twig environment singleton', function() {
+    expect($this->app->hasSingleton(Environment::class))->toBeTrue();
+});
+
+it('registers twig filesystem loader singleton', function() {
+    expect($this->app->hasSingleton(FilesystemLoader::class))->toBeTrue();
+});
+
+it('aliases twig loader interface to twig filesystem loader class', function() {
+    expect($this->app->hasAlias(LoaderInterface::class))->toBeTrue();
+    expect($this->app->getAlias(LoaderInterface::class))->toBe(FilesystemLoader::class);
+});
+
+it('sets twig filesystem loader paths', function() {
+    expect($this->app->get(FilesystemLoader::class)->getPaths())->toBe([$this->app->getViewsPath()]);
+});
+
+it('sets twig environment cache in production', function() {
+    $this->app->config(['app.env' => 'production']);
+    $this->app->bootService(new ViewService($this->app));
+
+    expect($this->app->get(Environment::class)->getCache())->toBe($this->app->getCachePath());
+});
+
+it('sets twig environment to debug when not in production', function() {
+    $this->app->config(['app.env' => 'local']);
+    $this->app->bootService(new ViewService($this->app));
+
+    expect($this->app->get(Environment::class)->isDebug())->toBeTrue();
+});
