78754005c7dc9f9fd5504ac1917fb47a2e23ec74
Aaron <aaron@ironmthome.com>
2021-11-15 22:16:45 -0500

    Add prefersJson, prefersHtml, hasHeader, setHeader, getHeader methods

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -17,19 +17,60 @@ class Response implements ResponseInterface
         $this->responseCode = $code;
     }
 
-    public function renderBodyToJson(): void
+    public function prefersJson(): bool
     {
-        echo json_encode($this->body);
+        return $this->hasHeader('Content-Type') && in_array($this->getHeader('Content-Type'), ['application/json']);
     }
 
-    public function renderBodyAsHtml(): void
+    public function prefersHtml(): bool
     {
-        echo $this->body;
+        return $this->hasHeader('Content-Type') && in_array($this->getHeader('Content-Type'), ['text/html']);
+    }
+
+    public function header(string $headerKey, ?string $headerValue = null)
+    {
+        if(is_null($headerValue)) {
+            return $this->getHeader($header);
+        }
+        $this->setHeader($headerKey, $headerValue);
+    }
+
+    public function hasHeader(string $header): bool
+    {
+        return array_key_exists($this->normalizeHeader($header), $this->headers);
+    }
+
+    public function setHeader(string $headerKey, string $headerValue): void
+    {
+        $this->headers[$this->normalizeHeader($headerKey)] = $headerValue;
+    }
+
+    public function getHeader(string $header): ?string
+    {
+        return $this->hasHeader($header) ? $this->headers[$this->normalizeHeader($header)] : null
+    }
+
+    private function normalizeHeader(string $header): string
+    {
+        $header = trim($header);
+        if($header === '') {
+            return $header;
+        }
+        $delim = strpos($header, '-') !== false ? '-' : (strpos($header, '_') !== false ? '_' : null);
+        if(!is_null($delim)) {
+            $headerParts = explode($delim, $header);
+            $headerStart = ucfirst(strtolower(array_shift($headerParts)));
+            return array_reduce($headerParts, function($carry, $part) {
+                return $carry . '-' . ucfirst(strtolower($item));
+            }, $headerStart);
+        }
+
+        return ucfirst(strtolower($header));
     }
 
     public function renderBody()
     {
-        $this->renderBodyToJson();
+        echo json_encode($this->body);
     }
 
     public function render()
